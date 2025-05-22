595603a0ff4593580cf01e321ad99462e3b99f5e
josemorales-g360 <Jose_Morales@cw.groups360.com>
2025-05-21 21:25:37 -0600

    Fix translation rendering by changing outerHTML to innerHTML in hersoTranslator function

--- src/translator.ts
+++ src/translator.ts
@@ -28,7 +28,7 @@ export const hersoTranslator = (dictionary: any) => {
                 element.placeholder = translation;
             }
         } else {
-            element.outerHTML = translation;
+            element.innerHTML = translation;
         }
     });
 }
